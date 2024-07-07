b6bae7f50399f4fc18566e4f819c039e0785f81c
SREESANKAR-P <sreesankarputhumana@gmail.com>
2024-07-07 13:01:08 +0530

    create header component in user

--- src/routes/UserRoute.jsx
+++ src/routes/UserRoute.jsx
@@ -5,10 +5,12 @@ import UserSignIn from "../pages/user/UserSignIn";
 import UserSignUp from "../pages/user/UserSignUp";
 import UserOtp from "../pages/user/UserOtp";
 import DoctorsList from "../pages/user/DoctorsList";
+import Header from "../component/user/Header";
 
 export default function UserRoute() {
   return (
     <>
+      <Header />
       <Routes>
         <Route path="" element={<UserHome />} />
         <Route path="login" element={<UserSignIn />} />
