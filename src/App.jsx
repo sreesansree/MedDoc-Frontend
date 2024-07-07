0ee524ef3d05885ac470725aa55418a01a230db7
SREESANKAR-P <sreesankarputhumana@gmail.com>
2024-07-07 10:03:32 +0530

    installed tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function App() {
-  return (
-    <div>App</div>
-  )
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
