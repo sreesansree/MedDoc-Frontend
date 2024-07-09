acd1fd850681f806e524448cd66165889e32e3be
SREESANKAR-P <sreesankarputhumana@gmail.com>
2024-07-09 23:29:05 +0530

    Partially completed Profile page

--- src/component/dashboard/DashProfile.jsx
+++ src/component/dashboard/DashProfile.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
-
+import React from "react";
+import { useSelector } from "react-redux";
 export default function DashProfile() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
-    <div>DashProfile</div>
-  )
+    <div className="max-w-lg mx-auto p-3 w-full">
+      <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
+      <formv className="flex flex-col">
+        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
+          <img
+            src={currentUser?.user?.profilePicture}
+            alt="user"
+            className="rounded-full w-full h-full border-8 border-[lightgray]"
+          />
+        </div>
+      </formv>
+    </div>
+  );
 }
